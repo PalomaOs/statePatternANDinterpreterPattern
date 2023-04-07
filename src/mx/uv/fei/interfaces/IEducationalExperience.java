bf4c70e60d6e269621283a959f34d39065eaba6a
PalomaOs <momaosiris@gmail.com>
2023-04-06 19:45:36 -0500

    agregue la interfaz profesor

---
+++ src/mx/uv/fei/interfaces/IEducationalExperience.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package mx.uv.fei.interfaces;
+
+import java.sql.SQLException;
+
+/**
+ *
+ * @author alexs
+ */
+public interface IEducationalExperience {
+
+    int registerEducationalExperience() throws SQLException;
+}
